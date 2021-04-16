c4f6eb9edfd939f5001aecbd0f1c5a6e65f96da5
Connor Gorr <xcongiex@gmail.com>
2021-04-16 14:24:40 -0700

    changed player schema and dto to include floorLevel and dex.

--- src/player/player.dto.ts
+++ src/player/player.dto.ts
@@ -3,5 +3,7 @@ export class PlayerDTO {
     readonly name: string;
     readonly hp: number;
     readonly attack: number;
+    readonly dex: number;
     readonly pageId: number;
+    readonly floorLevel: number;
 }
